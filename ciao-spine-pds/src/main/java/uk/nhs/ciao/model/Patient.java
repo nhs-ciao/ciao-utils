a5b96e4ce819028fed68a44a0475d6ec3b6ed9f3
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-11-19 14:21:26 +0000

    Updated Patient to implement Emptiable Added equals / hashcode methods

--- ciao-spine-pds/src/main/java/uk/nhs/ciao/model/Patient.java
+++ ciao-spine-pds/src/main/java/uk/nhs/ciao/model/Patient.java
@@ -1,11 +1,19 @@
 package uk.nhs.ciao.model;
 
-import java.util.ArrayList;
+import static uk.nhs.interoperability.payloads.util.Emptiables.isNullOrEmpty;
+
+import java.util.List;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 
 import uk.nhs.interoperability.payloads.HL7Date;
 import uk.nhs.interoperability.payloads.commontypes.Address;
 import uk.nhs.interoperability.payloads.commontypes.PersonName;
 import uk.nhs.interoperability.payloads.commontypes.Telecom;
+import uk.nhs.interoperability.payloads.util.Emptiable;
 import uk.nhs.interoperability.payloads.vocabularies.generated.Sex;
 
 /**
@@ -13,79 +21,141 @@ import uk.nhs.interoperability.payloads.vocabularies.generated.Sex;
  * then be used to link together routes which require details about a patient
  * @author Adam Hatherly
  */
-public class Patient {
-	
+public class Patient implements Emptiable {
 	private String nhsNumber;
 	private PersonName name;
-	private ArrayList<Address> address;
+	private List<Address> address;
 	private HL7Date dateOfBirth;
 	private HL7Date dateOfDeath;
 	private Sex gender;
-	private ArrayList<Telecom> telecom;
+	private List<Telecom> telecom;
 	private String practiceCode;
 	
 	public String getNhsNumber() {
 		return nhsNumber;
 	}
-	public void setNhsNumber(String nhsNumber) {
+
+	public void setNhsNumber(final String nhsNumber) {
 		this.nhsNumber = nhsNumber;
 	}
+
 	public PersonName getName() {
 		return name;
 	}
-	public void setName(PersonName name) {
+
+	public void setName(final PersonName name) {
 		this.name = name;
 	}
 	
 	public HL7Date getDateOfBirth() {
 		return dateOfBirth;
 	}
-	public void setDateOfBirth(HL7Date dateOfBirth) {
+
+	public void setDateOfBirth(final HL7Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
+
 	public HL7Date getDateOfDeath() {
 		return dateOfDeath;
 	}
-	public void setDateOfDeath(HL7Date dateOfDeath) {
+
+	public void setDateOfDeath(final HL7Date dateOfDeath) {
 		this.dateOfDeath = dateOfDeath;
 	}
-	public ArrayList<Telecom> getTelecom() {
+
+	public List<Telecom> getTelecom() {
 		return telecom;
 	}
-	public void setTelecom(ArrayList<Telecom> telecom) {
+
+	public void setTelecom(final List<Telecom> telecom) {
 		this.telecom = telecom;
 	}
-	public void addTelecom(Telecom telecom) {
-		if (this.telecom == null) {
-			this.telecom = new ArrayList<Telecom>();
+
+	public void addTelecom(final Telecom telecom) {
+		if (isNullOrEmpty(telecom)) {
+			return;
+		}
+		else if (this.telecom == null) {
+			this.telecom = Lists.newArrayList();
 		}
 		this.telecom.add(telecom);
 	}
+
 	public String getPracticeCode() {
 		return practiceCode;
 	}
-	public void setPracticeCode(String practiceCode) {
+
+	public void setPracticeCode(final String practiceCode) {
 		this.practiceCode = practiceCode;
 	}
-	public ArrayList<Address> getAddress() {
+
+	public List<Address> getAddress() {
 		return address;
 	}
-	public void setAddress(ArrayList<Address> address) {
+
+	public void setAddress(final List<Address> address) {
 		this.address = address;
 	}
-	public void addAddress(Address address) {
-		if (this.address == null) {
-			this.address = new ArrayList<Address>();
+
+	public void addAddress(final Address address) {
+		if (isNullOrEmpty(address)) {
+			return;
+		}
+		else if (this.address == null) {
+			this.address = Lists.newArrayList();
 		}
 		this.address.add(address);
 	}
+
 	public Sex getGender() {
 		return gender;
 	}
-	public void setGender(Sex gender) {
+
+	public void setGender(final Sex gender) {
 		this.gender = gender;
 	}
 
+	@Override
+	public boolean isEmpty() {
+		return isNullOrEmpty(nhsNumber) &&
+				isNullOrEmpty(name) &&
+				isNullOrEmpty(address) &&
+				isNullOrEmpty(dateOfBirth) &&
+				isNullOrEmpty(dateOfDeath) &&
+				gender == null &&
+				isNullOrEmpty(telecom) &&
+				isNullOrEmpty(practiceCode);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Strings.nullToEmpty(nhsNumber).hashCode();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * Only the nhsNumber is used to test for equality
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		
+		final Patient other = (Patient)obj;
+		return Objects.equal(Strings.emptyToNull(nhsNumber), Strings.emptyToNull(other.nhsNumber));
+	}
+	
+	@Override
+	public String toString() {
+		// Only adding nhsNumber
+		return MoreObjects.toStringHelper(this)
+			.add("nhsNumber", Strings.nullToEmpty(nhsNumber))
+			.toString();
+	}
 	
 	/*name.addGivenSimple("Adam");
 	name.addFamilySimple("Hatherly");
